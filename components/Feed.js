d589dde31b88eaf189219acd0ae2b033aacfb0da
Manish Rawat <github.jmtss@gmail.com>
2021-10-28 13:01:48 +0530

    done with stories

--- components/Feed.js
+++ components/Feed.js
@@ -1,10 +1,13 @@
+import Stories from './Stories';
+
 function Feed() {
   return (
-    <main>
+    <main className='grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto'>
       {/*Sections  */}
-      <section>
-        {/* Section */}
+      <section className='col-span-2'>
         {/* stories */}
+        <Stories />
+        {/* Post */}
       </section>
       {/* Sections */}
       <section>
