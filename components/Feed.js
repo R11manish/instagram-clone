b8f825bd88f670ddbe74696b4865e13c7a35ec0b
Manish Rawat <github.jmtss@gmail.com>
2021-10-28 10:57:35 +0530

    header completed

---
+++ components/Feed.js
@@ -0,0 +1,18 @@
+function Feed() {
+  return (
+    <main>
+      {/*Sections  */}
+      <section>
+        {/* Section */}
+        {/* stories */}
+      </section>
+      {/* Sections */}
+      <section>
+        {/* Mini Profile */}
+        {/* Suggestions */}
+      </section>
+    </main>
+  );
+}
+
+export default Feed;
